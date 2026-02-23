a9b7d323536ac6c5c783b198109caaa26de144c0
Absolute <ssmolovoi2000@gmail.com>
2026-02-24 01:46:40 +0200

    feat(3-5-events-filter-bar-and-reactive-filtering): story complete

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { EventFormComponent } from './features/event-form/event-form.component';
+import { EventsFilterComponent } from './features/events-filter/events-filter.component';
 import { EventsTableComponent } from './features/events-table/events-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [EventFormComponent, EventsTableComponent],
+  imports: [EventFormComponent, EventsFilterComponent, EventsTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
